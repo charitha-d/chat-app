fe819b8609c20c1191f94a35b080dc617539bf7d
charitha-d <charithareddy705@gmail.com>
2025-06-28 16:11:53 +0530

    all

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,14 +1,14 @@
 import { useAuthContext } from "../../context/AuthContext";
 import { extractTime } from "../../utils/extractTime";
-import useConversation from "../../zustand/useConversation";
+
 import user_account from '../sidebar/user_account.png'
 const Message = ({ message }) => {
 	const { authUser } = useAuthContext();
-	const { selectedConversation } = useConversation();
+	
 	const fromMe = message.senderId === authUser._id;
 	const formattedTime = extractTime(message.createdAt);
 	const chatClassName = fromMe ? "chat-end" : "chat-start";
-	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+	
 	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
 
 	const shakeClass = message.shouldShake ? "shake" : "";
